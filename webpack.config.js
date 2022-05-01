294f6f6af48b0e6806e3d05dc06fc1b671591d42
Pablo <pablo.jara@adataart.com>
2022-05-01 04:26:30 -0300

    fix webpack y babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,33 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const devMode = process.env.NODE_ENV !== 'production';
 
-module.exports={
-    entry:[
+module.exports = {
+    watch: true,
+    watchOptions: {
+        //Files or folders that are not monitored
+        ignored: /node_modules/,
+        //After listening to the change, it will wait for 300ms to execute the action, so as to prevent the file from being updated too fast, resulting in too high recompilation frequency  
+        aggregateTimeout: 300,
+        //To judge whether the file has changed or not is realized by constantly asking the system whether the specified file has changed or not
+        poll: 1000
+    },
+    entry: [
         './src/app/index.js'
-    ], 
-    output:{
-        path : __dirname + '/build',
-        filename : 'bundle.js'
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
     },
     mode: 'development',
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.js$/,
                 loader: 'babel-loader',
             },
             {
                 test: /\.css$/,
-                use:[
-                    devMode ? 'style-loader': MiniCssExtractPlugin.loader,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
                     'css-loader'
                 ]
             }
