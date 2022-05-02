7c819b541c4f8a26e9899181423585bfc1a65c3f
Pablo <pablo.jara@adataart.com>
2022-05-02 05:13:18 -0300

    add lint

--- src/app/components/WeatherForm.js
+++ src/app/components/WeatherForm.js
@@ -1,23 +1,24 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React from 'react'
 
 const WeatherForm = (props) => (
-  <div className="card card-body">
-    <form onSubmit={props.getWeather}>
-      <div className="form-group">
-        <select className="form-select" name="ciudad" aria-label="ciudad" placeholder="Ciudad" autoFocus>
-        <option value="" >Elige una ciudad</option>
-          <option value="quito" >Quito</option>
-          <option value="buenos aires">Buenos Aires</option>
-          <option value="paris">Paris</option>
-          <option value="london">Londres</option>
-          <option value="kiev">Kiev</option>
-        </select>
+	<div className="card card-body">
+		<form onSubmit={props.getWeather}>
+			<div className="form-group">
+				<select className="form-select" name="ciudad" aria-label="ciudad" placeholder="Ciudad" autoFocus>
+					<option value="" >Elige una ciudad</option>
+					<option value="quito" >Quito</option>
+					<option value="buenos aires">Buenos Aires</option>
+					<option value="paris">Paris</option>
+					<option value="london">Londres</option>
+					<option value="kiev">Kiev</option>
+				</select>
         
-      </div>
+			</div>
  
-      <button className="btn btn-outline-light w-100 mt-2">Buscar</button>
-    </form>
-  </div>
-);
+			<button className="btn btn-outline-light w-100 mt-2">Buscar</button>
+		</form>
+	</div>
+)
 
-export default WeatherForm;
+export default WeatherForm
--- src/app/keys.js
+++ src/app/keys.js
@@ -1,3 +1,1 @@
-module.exports={
-    WEATHER_KEY: '0928382946a702f82cd53a91fc37e782'
-}
+export const WEATHER_KEY = '0928382946a702f82cd53a91fc37e782'
